b8416f46c4f28a9be6b8833563106a7922d4cca5
Your Name <you@example.com>
2015-07-15 15:31:03 +0800

    Testing

--- src/android/Nfc.java
+++ src/android/Nfc.java
@@ -5,22 +5,33 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class Nfc extends CordovaPlugin {
+    private final String ACTION_INIT = "init";
+
+    private String password;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
-
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
+        }
+        else if (action.equals(ACTION_INIT)){
+            init(callbackContext);
+        }
+        else {
 
-            return true;
-
-        } else {
-            
             return false;
 
         }
+
+        return true;
+    }
+
+    private void init(CallbackContext callbackContext){
+        password = "1234";
+
+        callbackContext.success();
     }
 }
--- www/nfc.js
+++ www/nfc.js
@@ -3,5 +3,8 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Nfc", "greet", [name]);
+    },
+    init: function (successCallback, erroCallback) {
+        cordova.exec(successCallback, errorCallback, "Nfc", "init", []);
     }
 };
