700cebc4c20db764b152ce61012dcaa07e80ccfe
Your Name <you@example.com>
2015-07-15 14:35:52 +0800

    Testing

--- src/android/Nfc.java
+++ src/android/Nfc.java
@@ -1,25 +1,27 @@
 package com.dnrps.com;
  
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.json.JSONObject;
+import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class Nfc extends CordovaPlugin {
-     public static final String ACTION_INIT = "init"; 
-    
+    public static final String ACTION_INIT = "init"; 
+
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
         if (action.equals(ACTION_INIT)) {
+
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
 
             return true;
+
         } else {
-            callbackContext.error("Invalid action");
+            
             return false;
+
         }
     }
 }
