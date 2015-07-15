806b87c097b857e5bc95dae33b5d973796b64abc
Your Name <you@example.com>
2015-07-15 17:07:49 +0800

    testing

--- src/android/Nfc.java
+++ src/android/Nfc.java
@@ -1,6 +1,12 @@
 package com.dnrps.nfc;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+
+import com.nxp.nfclib.ntag.NTag213215216;
 import com.nxp.nfcliblite.Interface.NxpNfcLibLite;
+import com.nxp.nfcliblite.Interface.Nxpnfcliblitecallback;
 
 import org.apache.cordova.*;
 import org.json.JSONArray;
@@ -9,7 +15,10 @@ import org.json.JSONException;
 public class Nfc extends CordovaPlugin {
     private final String ACTION_INIT = "init";
 
+    private static String TAG = "NfcPlugin";
+
     private String password;
+    private Tag tagInfo;
 
     /** Create lib lite instance. */
     private NxpNfcLibLite libInstance = null;
@@ -23,6 +32,32 @@ public class Nfc extends CordovaPlugin {
         libInstance.registerActivity(cordova.getActivity());
 
         System.out.println("Plugin Initialized.");
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        
+        System.out.println("onNewIntent Intent: " + intent.toString());
+        System.out.println("onNewIntent Action: " + intent.getAction());
+
+        tagInfo = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        // Only act on intents from a tag
+        if (tagInfo == null) {
+            return;
+        }
+
+        System.out.println("Tag info: " + tagInfo.toString());
+
+        Nxpnfcliblitecallback callback = new Nxpnfcliblitecallback() {
+            @Override
+            public void onNTag213215216CardDetected(NTag213215216 nTag213215216) {
+
+            }
+        };
+
+        libInstance.filterIntent(intent, callback);
     }
 
     @Override
@@ -47,6 +82,11 @@ public class Nfc extends CordovaPlugin {
 
     private void init(CallbackContext callbackContext){
         password = "1234";
+
+        if (NxpNfcLibLite.getInstance() != null) {
+            System.out.println("Starting startForeGroundDispatch in init");
+            libInstance.startForeGroundDispatch();
+        }
 
         callbackContext.success();
     }
