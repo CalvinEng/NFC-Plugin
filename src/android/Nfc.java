8ce921439f2cde397bdb276edcfb372293a3cdc5
Your Name <you@example.com>
2015-07-15 15:06:54 +0800

    Testing

--- src/android/Nfc.java
+++ src/android/Nfc.java
@@ -4,7 +4,7 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-public class Hello extends CordovaPlugin {
+public class Nfc extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
