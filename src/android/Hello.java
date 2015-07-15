56789575ad47fc91e7bec8be15b619468d422b4b
Your Name <you@example.com>
2015-07-15 14:56:19 +0800

    Testing

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -1,10 +1,10 @@
-package com.dnrps.com;
- 
+package com.example.plugin;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-public class Nfc extends CordovaPlugin {
+public class Hello extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
--- www/nfc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Nfc", "greet", [name]);
-    }
-};
