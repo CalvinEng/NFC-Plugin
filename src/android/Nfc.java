47d66de48f21da8460d50fec00bfdf0e19de1fa2
Your Name <you@example.com>
2015-07-15 16:48:41 +0800

    testing

--- src/android/Nfc.java
+++ src/android/Nfc.java
@@ -1,5 +1,7 @@
 package com.dnrps.nfc;
 
+import com.nxp.nfcliblite.Interface.NxpNfcLibLite;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -8,6 +10,20 @@ public class Nfc extends CordovaPlugin {
     private final String ACTION_INIT = "init";
 
     private String password;
+
+    /** Create lib lite instance. */
+    private NxpNfcLibLite libInstance = null;
+
+    @Override
+    public void pluginInitialize() {
+        super.pluginInitialize();
+
+        libInstance = NxpNfcLibLite.getInstance();
+        // Call registerActivity function before using other functions of the library.
+        libInstance.registerActivity(cordova.getActivity());
+
+        webView.loadUrl("javascript:console.log('Initialize Plugin');");
+    }
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
