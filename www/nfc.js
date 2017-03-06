6aa681232765bf7f492f9442d9c5a23342164593
Your Name <you@example.com>
2017-03-06 12:14:52 +0800

    Compatible with Ionic 2

--- www/nfc.js
+++ www/nfc.js
@@ -1,10 +1,20 @@
-/*global cordova, module*/
+function Nfc() {}
 
-module.exports = {
-    init: function (user, url, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Nfc", "init", [user, url]);
-    },
-    write: function (message, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Nfc", "write", [message]);
-    }
+Nfc.prototype.init = function (user, url, successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "Nfc", "init", [user, url]);
 };
+
+Nfc.prototype.write: function (message, successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "Nfc", "write", [message]);
+};
+
+Nfc.install = function () {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+
+    window.plugins.Nfc = new Nfc();
+    return window.plugins.Nfc;
+};
+
+cordova.addConstructor(Nfc.install);
